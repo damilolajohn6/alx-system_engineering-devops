1f805871887549636f0ee837a63bc6476bdc3992
damilolajohn6 <damilolajohn622@gmail.com>
2023-08-16 18:44:00 +0100

    user limits

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Adjust the OS configuration to enable successful login for the holberton user and error-free file opening
+
+exec { 'update soft limit for holberton user':
+    command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+    provider => shell,
+}
+
+exec { 'update hard limit for holberton user':
+    command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
+    provider => shell,
+}
